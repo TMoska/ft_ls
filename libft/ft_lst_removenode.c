abc169e00339000837c78c4739de4caf3b13dabf
Alessandro Marzialetti <setti93@gmail.com>
2017-01-21 13:15:46 +0100

    Well, memory leaks on remove node

--- libft/ft_lst_removenode.c
+++ libft/ft_lst_removenode.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 18:11:17 by moska             #+#    #+#             */
-/*   Updated: 2017/01/14 16:47:57 by tmoska           ###   ########.fr       */
+/*   Updated: 2017/01/21 13:13:29 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void		ft_lst_removenode(t_list **begin_list, t_list *removable)
 		{
 			old = *begin_list;
 			*begin_list = (*begin_list)->next;
+			free(old);
 		}
 		else
 			ft_lst_removenode(&((*begin_list)->next), removable);
