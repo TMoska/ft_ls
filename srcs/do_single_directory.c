f2c3d89499424ac648da78d0691cd126a494116a
TMoska <tomas.moska@gmail.com>
2017-02-06 05:39:29 +0100

    fix some pointers in loop checking

--- srcs/do_single_directory.c
+++ srcs/do_single_directory.c
@@ -6,7 +6,7 @@
 /*   By: moska <moska@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/14 22:30:46 by tmoska            #+#    #+#             */
-/*   Updated: 2017/02/06 05:20:33 by moska            ###   ########.fr       */
+/*   Updated: 2017/02/06 05:35:51 by moska            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,17 @@ static void		traceback_link_path(char ***split_link, char ***split_path,
 	}
 }
 
-int	link_will_loop(char **split_link, char **split_path, char link[BUFF], int i)
+int	link_will_loop(char ***split_link, char ***split_path, char link[BUFF], int i)
 {
-	if (ft_strequ(split_path[i - 1], *split_link) ||
+	if (ft_strequ((*split_path)[i - 1], **split_link) ||
 		ft_strequ(link, "./") || ft_strequ(link, "."))
 	{
-
-		del_deep_char(split_path);
-		del_deep_char(split_link);
+		del_deep_char(*split_path);
+		del_deep_char(*split_link);
 		return (1);
 	}
-	del_deep_char(split_path);
-	del_deep_char(split_link);
+	del_deep_char(*split_path);
+	del_deep_char(*split_link);
 	return (0);
 }
 
@@ -72,7 +71,7 @@ static t_bool	link_to_self(t_file *file)
 		link[lu] = '\0';
 		split_link = ft_strsplit(link, '/');
 		traceback_link_path(&split_link, &split_path, &i, file);
-		return (link_will_loop(split_path, split_link, link, i));
+		return (link_will_loop(&split_link, &split_path, link, i));
 	}
 	return (0);
 }
